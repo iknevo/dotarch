e997ef1879de9a8b1cbc59f660e2210d0a19ffdd
iknevo <nevoexists@gmail.com>
2025-09-27 14:46:57 +0300

    update

---
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,12 @@
+return {
+  { "rebelot/kanagawa.nvim" },
+  { "catppuccin/nvim", name = "catppuccin" },
+  { "ellisonleao/gruvbox.nvim" },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "kanagawa-dragon", -- pick the default
+    },
+  },
+}
