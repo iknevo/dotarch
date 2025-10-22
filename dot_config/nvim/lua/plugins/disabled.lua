7b2d144eaf9e0e76efeee901f7fefbebf6d59ed5
Ahmed Abdelhafiez <nevoexists@gmail.com>
2025-10-22 16:38:54 +0300

    update

--- dot_config/nvim/lua/plugins/disabled.lua
+++ dot_config/nvim/lua/plugins/disabled.lua
@@ -4,4 +4,8 @@ return {
     "akinsho/bufferline.nvim",
     enabled = false,
   },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    enabled = false,
+  },
 }
--- dot_config/nvim/lua/plugins/snacks.lua
+++ dot_config/nvim/lua/plugins/snacks.lua
@@ -1,11 +1,11 @@
 return {
-  "snacks.nvim",
+  "folke/snacks.nvim",
   opts = {
     picker = {
       sources = {
         files = {
-          hidden = false, -- show hidden files if you want
-          ignored = true, -- respect ignore patterns
+          hidden = false,
+          ignored = true,
           exclude = { "node_modules", ".git", "dist", "build", ".next" },
         },
         grep = {
@@ -21,25 +21,7 @@ return {
       },
     },
     dashboard = {
-      preset = {
-        pick = function(cmd, opts)
-          return LazyVim.pick(cmd, opts)()
-        end,
-        header = [[ ]],
-        -- stylua: ignore
-        ---@type snacks.dashboard.Item[]
-        keys = {
-          { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
-          { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
-          { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
-          { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
-          { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
-          { icon = " ", key = "s", desc = "Restore Session", section = "session" },
-          { icon = " ", key = "x", desc = "Lazy Extras", action = ":LazyExtras" },
-          { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
-          { icon = " ", key = "q", desc = "Quit", action = ":qa" },
-        },
-      },
+      enabled = false,
     },
   },
 }
