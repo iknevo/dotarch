2815a107e7abe5b8f90595112029f50255ea44d3
Ahmed Abdelhafiez <nevoexists@gmail.com>
2026-01-09 12:39:07 +0200

    sync

--- dot_config/omarchy/themes/nevo/neovim.lua
+++ dot_config/omarchy/themes/nevo/neovim.lua
@@ -10,3 +10,38 @@ return {
 		},
 	},
 }
+-- return {
+-- 	{
+-- 		"bjarneo/aether.nvim",
+-- 		name = "aether",
+-- 		priority = 1000,
+-- 		opts = {
+-- 			disable_italics = false,
+-- 			colors = {
+-- 				base00 = "#000000", -- Default background
+-- 				base01 = "#000000", -- Lighter background (status bars)
+-- 				base02 = "#0e6571", -- Selection background
+-- 				base03 = "#5b858b", -- Comments, invisibles
+-- 				base04 = "#87a7ab", -- Dark foreground
+-- 				base05 = "#b2cacd", -- Default foreground
+-- 				base06 = "#c1d4d7", -- Light foreground
+-- 				base07 = "#ebfdff", -- Light background
+--
+-- 				base08 = "#e34e1c", -- Variables, errors, red
+-- 				base09 = "#7060eb", -- Integers, constants, orange
+-- 				base0A = "#d67e5c", -- Classes, types, yellow
+-- 				base0B = "#78ccb7", -- Strings, green
+-- 				base0C = "#40d4e7", -- Support, regex, cyan
+-- 				base0D = "#16a3b6", -- Functions, keywords, blue
+-- 				base0E = "#ff5900", -- Keywords, storage, magenta
+-- 				base0F = "#d5971a", -- Deprecated, brown/yellow
+-- 			},
+-- 		},
+-- 	},
+-- 	{
+-- 		"LazyVim/LazyVim",
+-- 		opts = {
+-- 			colorscheme = "aether",
+-- 		},
+-- 	},
+-- }
